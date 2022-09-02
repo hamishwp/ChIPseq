b7a4e75bffce0b4b7a28fa7d80b7979241e90a48
Hamish Patten <hamishpatten@hotmail.co.uk>
2022-09-02 08:03:59 +0200

    Finalised Reporting of the results in the Main.Rmd file.

--- Code/Packages.R
+++ Code/Packages.R
@@ -3,21 +3,22 @@
 ###########################################################################
 if(installer){
   # # Now let's install all the other necessary packages:
-  list.of.packages <- c("ggplot2","tidyverse","caret","stringr","keras","tensorflow",
-                        "FactoMineR","factoextra","gridExtra","Rgtsvm")
+  list.of.packages <- c("ggplot2","tidyverse","caret","stringr","reticulate",
+                        "tensorflow","keras",
+                        "FactoMineR","factoextra","gridExtra")
   # # Install the packages that aren't already installed
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) install.packages(new.packages)
   
-  # install_tensorflow(envname = "r-reticulate",version = "cpu")
-  # install_keras(envname = "r-reticulate",version = "cpu")
-  # virtualenv_create("r-reticulate", python = "/home/hamishwp/miniconda3/bin/python3.9")
-  # library(tensorflow)
+  install_tensorflow(envname = "r-reticulate",version = "cpu")
+  install_keras(envname = "r-reticulate",version = "cpu")
+  virtualenv_create("r-reticulate", python = "/home/hamishwp/miniconda3/bin/python3.9")
+  library(tensorflow)
   # install_tensorflow(envname = "r-reticulate")
-  # library(keras)
+  library(keras)
   # install_keras(envname = "r-reticulate")
-  # library(tensorflow)
-  # tf$constant("Hello Tensorflow!")
+  library(tensorflow)
+  tf$constant("Hello Tensorflow!")
   
 }
 ###########################################################################
